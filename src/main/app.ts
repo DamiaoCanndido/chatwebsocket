a2e74eafc95ed0ac9015aa954fb275734c3be369
Damiao <tower_gamer@hotmail.com>
2021-04-26 22:01:12 -0300

    Chore: install socket.io

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 import "../database";
 import { settingsRouter } from "../routes/settings";
 import { usersRouter } from "../routes/users";
@@ -7,10 +9,17 @@ import { messagesRouter } from "../routes/messages";
 
 const app = express();
 
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+    console.log("Se conectou", socket.id);
+})
+
 app.use(express.json());
 
 app.use(settingsRouter);
 app.use(usersRouter);
 app.use(messagesRouter);
 
-export { app };
+export { http };
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,3 @@
-import { app } from "./app";
+import { http } from "./app";
 
-app.listen(3333, () => console.log("SERVIDOR ONLINE!"));
+http.listen(3333, () => console.log("SERVIDOR ONLINE!"));
